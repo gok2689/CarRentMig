21945e2363b2ed7620a94ca7163fc4d0dfde1eab
goktug <goktug.dulkan@gmail.com>
2014-09-04 17:41:57 +0300

    https://github.com/gok2689/CarRentMig.git

---
+++ CRMDomain/Event.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMDomain
+{
+    public class Event : TableEntity, ITableEntity
+    {
+    }
+}
